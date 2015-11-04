3715193179095bf578b7b0cca81a4ae96fd328cb
Alexandre Nourissier <alexandre.nourissier@outlook.com>
2015-11-04 18:47:14 +0100

    TD 1 - FIZZBUZZ

---
+++ 4NET - TD 1 - FIZZBUZZ/ConsoleApplication1/InputList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class InputList
+    {
+        public class MemberAddedEventArgs : EventArgs
+        {
+            public int MemberAdd { get; set; }
+        }
+
+        private List<int> list;
+
+        public InputList(List<int> list)
+        {
+            this.list = list;
+        }
+
+        public void Add(int x)
+        {
+            list.Add(x);
+            var args = new MemberAddedEventArgs { MemberAdd = x };
+            OnMemberAdded(args);
+        }
+
+        protected virtual void OnMemberAdded(MemberAddedEventArgs e)
+        {
+            EventHandler<MemberAddedEventArgs> handler = MemberAdded;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<MemberAddedEventArgs> MemberAdded;
+
+    }
+
+  
+
+}
---
+++ 4NET - TD 1 - FIZZBUZZ/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //On initialise la suite de nombre 
+            var numbers = new InputList(new List<int>());
+            numbers.MemberAdded += numbers_memberadded;
+            PopulateList(numbers, 1, 20);
+            
+
+
+            Console.ReadLine();
+        }
+
+
+        private static void numbers_memberadded(object sender, InputList.MemberAddedEventArgs e)
+        {
+            var output = "";
+            if (e.MemberAdd % 3 == 0)
+            {
+                output += "fizz";
+            }
+            if (e.MemberAdd % 5 == 0)
+            {
+                output += "buzz";
+            }
+            if (string.IsNullOrEmpty(output))
+            {
+                output += e.MemberAdd;
+            }
+            Console.WriteLine(output);
+        }
+
+
+
+        public static void PopulateList(InputList inputList, int min, int max)
+        {
+            for (var i = min; i <= max; i++)
+            {
+                inputList.Add(i);
+            }
+        }
+    }
+  
+
+
+
+}
