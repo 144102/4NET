3ab2058f75b85deeb5aba000e4f7cf0a5548bf6d
Alexandre Nourissier <alexandre.nourissier@outlook.com>
2015-11-05 11:00:32 +0100

    TD 3 - WCF

---
+++ 4NET - TD 3 - WCF/WcfService2/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService2
+{
+    [DataContract]
+    public class Person
+    {
+        private string firstName;
+
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+        [DataMember]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private int age;
+
+        [DataMember]
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        
+    }
+}
---
+++ 4NET - TD 3 - WCF/WcfService2/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public static List<Person> Persons = new List<Person>();
+
+
+        void IService1.CreatePerson(Person person)
+        {
+            if (person != null)
+            {
+                Persons.Add(person);
+            }
+        }
+
+        Person IService1.GetPerson()
+        {
+            return Persons.FirstOrDefault();
+        }
+
+        List<Person> IService1.GetPersons()
+        {
+            return Persons;
+        }
+    }
+}
