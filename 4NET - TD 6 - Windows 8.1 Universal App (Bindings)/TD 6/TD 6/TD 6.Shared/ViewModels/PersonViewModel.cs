6523cda4f62af0649956b255d569e7f9d2da2b45
Alexandre Nourissier <alexandre.nourissier@outlook.com>
2015-12-10 09:57:03 +0100

    TD 6 - Windows 8.1 Universal Apps Bindings

---
+++ 4NET - TD 6 - Windows 8.1 Universal App (Bindings)/TD 6/TD 6/TD 6.Shared/ViewModels/PersonViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using TD_6.Models;
+
+namespace TD_6.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        private string name = "MyName";
+        public string Name
+        {
+            get{ return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        private ObservableCollection<Person> persons = new ObservableCollection<Person>();
+
+        public ObservableCollection<Person> Persons
+        {
+            get { return persons; }
+            set
+            {
+                persons = value;
+                RaisePropertyChanged("Persons");
+            }
+        }
+
+
+
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        public PersonViewModel()
+        {
+            this.persons.Add(new Person()
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            });
+            this.persons.Add(new Person()
+            {
+                FirstName = "Jack",
+                LastName = "Harkness"
+            });
+        }
+
+        
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+    }
+}
---
+++ 4NET - TD 6 - Windows 8.1 Universal App (Bindings)/TD 6/TD 6/TD 6.Windows/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TD_6.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TD_6
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MainPage : Page
+    {
+
+        private PersonViewModel mainViewModel = new PersonViewModel();
+
+        public PersonViewModel MainViewModel
+        {
+            get { return mainViewModel; }
+            set { mainViewModel = value; }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            base.DataContext = mainViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainViewModel.Name += "1";
+            MainViewModel.Persons.Add(new Models.Person { FirstName="addedFN", LastName="addedLN"});
+        }
+    }
+}
