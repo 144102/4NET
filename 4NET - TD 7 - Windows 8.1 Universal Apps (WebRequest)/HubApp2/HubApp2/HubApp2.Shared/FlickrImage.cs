4fee0adaae4b13e9c7e2692001925be0fd865766
Alexandre Nourissier <alexandre.nourissier@outlook.com>
2015-12-11 10:15:01 +0100

    4NET - TD 7 - Windows 8.1 Universal Apps WebRequest

---
+++ 4NET - TD 7 - Windows 8.1 Universal Apps (WebRequest)/HubApp2/HubApp2/HubApp2.Shared/FlickrImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace HubApp2
+{
+    public class FlickrImage
+    {
+        public Uri uri { get; set; }
+        public BitmapImage bitmap { get; set; }
+    }
+}
---
+++ 4NET - TD 7 - Windows 8.1 Universal Apps (WebRequest)/HubApp2/HubApp2/HubApp2.Windows/SettingsFlyout1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
+
+namespace HubApp2
+{
+    public sealed partial class SettingsFlyout1 : SettingsFlyout
+    {
+        #region Properties
+        private DateTime lastUpdatedAt = DateTime.MinValue;
+        public DateTime LastUpdatedAt
+        {
+            get
+            {
+                return lastUpdatedAt;
+            }
+            set { lastUpdatedAt = value; }
+        }
+        
+        public string TxtboxName
+        {
+            get
+            {
+                return txtboxName.Text;
+            }
+        }
+
+        public double SldAmount
+        {
+            get
+            {
+                return sldAmount.Value;
+            }
+        }
+
+        public bool TglRefill
+        {
+            get
+            {
+                return tglRefill.IsOn;
+            }
+        }
+
+        #endregion
+        public SettingsFlyout1()
+        {
+            this.InitializeComponent();
+            sldAmount.Minimum = 0;
+            sldAmount.Maximum = 10;
+
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            var name = localSettings.Values["Name"];
+            var amount = localSettings.Values["Amount"];
+            var refill = localSettings.Values["Refill"];
+            var lastUpdatedAt = localSettings.Values["LastUpdatedAt"];
+
+            txtboxName.Text = name !=null ? (String)name :String.Empty  ;
+            sldAmount.Value = amount !=null? (double)amount : 0;
+            tglRefill.IsOn = refill !=null ?(bool)refill : false;
+            if(lastUpdatedAt != null)
+            {
+                txtbckLastUpdatedAt.Text =  DateTime.Parse((string)lastUpdatedAt).ToString();
+                txtbckLastUpdatedAt.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtbckLastUpdatedAt.Visibility = Visibility.Collapsed;
+            }
+            
+        }
+    }
+}
